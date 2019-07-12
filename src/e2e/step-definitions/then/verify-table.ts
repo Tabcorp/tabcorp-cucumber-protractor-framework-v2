84c523f2827d1083ef4dbc3d3274e03383fa3dad
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2019-07-12 10:49:27 +1000

    add-simple-table-contains-steps

--- src/e2e/step-definitions/then/verify-table.ts
+++ src/e2e/step-definitions/then/verify-table.ts
@@ -1,16 +1,11 @@
-import { assert } from 'chai';
-import { Then } from 'cucumber';
-import { TableDefinition } from 'cucumber';
+import { expect } from 'chai';
+import { TableDefinition, Then } from 'cucumber';
 import { WebElementHelper } from '../../support/framework-helpers/implementations/web-element-helper';
-import { HtmlHelper } from '../../support/framework-helpers/implementations/html-helper';
 import { RegistrationIoC } from '../../IoC/registration-ioc';
 import { BASETYPES } from '../../IoC/base-types';
-import { IComponentsWait } from '../../support/framework-helpers/interfaces/component-wait';
 import { StringManipulationHelper } from '../../support/steps-helpers/string-manipulation-helper';
 
 const elementHelper = (): WebElementHelper => RegistrationIoC.getContainer().get<WebElementHelper>(BASETYPES.WebElementHelper);
-const htmlHelper = (): HtmlHelper => RegistrationIoC.getContainer().get<HtmlHelper>(BASETYPES.HtmlHelper);
-const componentWait = (): IComponentsWait => RegistrationIoC.getContainer().get<IComponentsWait>(BASETYPES.ComponentsWait);
 const stringManipulationHelper = (): StringManipulationHelper => RegistrationIoC.getContainer().get<StringManipulationHelper>(BASETYPES.StringManipulationHelper);
 
 Then(/^the "([^"]*)" table contains the following:$/, async (elementName: string, dataTable: TableDefinition) => {
