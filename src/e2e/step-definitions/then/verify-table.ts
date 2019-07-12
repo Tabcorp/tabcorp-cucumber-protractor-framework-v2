e5d0f2b94aa52c6d0b98b391fe6f947bf99f2da7
Cameron Bradley <cameron.bradley@tabcorp.com.au>
2019-07-12 10:48:06 +1000

    add-simple-table-contains-steps

--- src/e2e/step-definitions/then/verify-table.ts
+++ src/e2e/step-definitions/then/verify-table.ts
@@ -6,15 +6,35 @@ import { HtmlHelper } from '../../support/framework-helpers/implementations/html
 import { RegistrationIoC } from '../../IoC/registration-ioc';
 import { BASETYPES } from '../../IoC/base-types';
 import { IComponentsWait } from '../../support/framework-helpers/interfaces/component-wait';
+import { StringManipulationHelper } from '../../support/steps-helpers/string-manipulation-helper';
 
 const elementHelper = (): WebElementHelper => RegistrationIoC.getContainer().get<WebElementHelper>(BASETYPES.WebElementHelper);
 const htmlHelper = (): HtmlHelper => RegistrationIoC.getContainer().get<HtmlHelper>(BASETYPES.HtmlHelper);
 const componentWait = (): IComponentsWait => RegistrationIoC.getContainer().get<IComponentsWait>(BASETYPES.ComponentsWait);
+const stringManipulationHelper = (): StringManipulationHelper => RegistrationIoC.getContainer().get<StringManipulationHelper>(BASETYPES.StringManipulationHelper);
 
 Then(/^the "([^"]*)" table contains the following:$/, async (elementName: string, dataTable: TableDefinition) => {
-  assert(false, 'STEP NOT IMPLEMENTED');
+  const element: ElementFinder = await elementHelper().getElementByCss(elementName);
+  return element.getText().then(function (text) {
+    dataTable.hashes().forEach(function (row) {
+      for (const key in row) {
+        var cucumber_text = stringManipulationHelper().replaceLineBreaks(row[key]);
+        var ui_text = stringManipulationHelper().replaceLineBreaks(text);
+        expect(ui_text).to.deep.include(cucumber_text);
+      }
+    });
+  });
 });
 
 Then(/^the "([^"]*)" table does not contain the following:$/, async (elementName: string, dataTable: TableDefinition) => {
-  assert(false, 'STEP NOT IMPLEMENTED');
+  const element: ElementFinder = await elementHelper().getElementByCss(elementName);
+  return element.getText().then(function (text) {
+    dataTable.hashes().forEach(function (row) {
+      for (const key in row) {
+        var cucumber_text = stringManipulationHelper().replaceLineBreaks(row[key]);
+        var ui_text = stringManipulationHelper().replaceLineBreaks(text);
+        expect(ui_text).to.not.deep.include(cucumber_text);
+      }
+    });
+  });
 });
